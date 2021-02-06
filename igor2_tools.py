372ec7c9c4bff308e96e697e22e1ef45dea32bc8
drJosh0 <62723593+drJosh0@users.noreply.github.com>
2021-02-06 08:32:30 -0500

    working on optimized igor2_tools logic

---
+++ igor2_tools.py
@@ -0,0 +1,26 @@
+import os
+import re
+
+from datetime import datetime, timezone
+
+class Search:
+
+    def __init__(self, filename):
+        self.file = filename
+
+    @property
+    def name(self):
+        pass
+
+    @property
+    def report(self):
+        pass
+
+    @property
+    def list(self):
+        pass
+
+def Visualize(data): #generate plots Vizualise(data) where data = Search(filename)
+    pass
+
+
